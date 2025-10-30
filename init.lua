d8a057e122cb553850107c8c2058fe61e77e8769
Paolo Stancato <paolostancato@users.noreply.github.com>
2025-10-30 11:55:11 -0300

    removed dashboard

--- init.lua
+++ init.lua
@@ -313,17 +313,6 @@ require('lazy').setup({
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
-  {
-    'goolord/alpha-nvim',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-      local startify = require 'alpha.themes.startify'
-      startify.file_icons.provider = 'devicons'
-      require('alpha').setup(startify.config)
-    end,
-  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
